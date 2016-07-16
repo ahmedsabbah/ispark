3d6874e99ec84279258a9e5a711fbbdf75c58b21
Ahmed Sabbah <ahmedsabba7@gmail.com>
2016-07-16 05:23:35 +0200

    frontend updated

--- contents/urls.py
+++ contents/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import include, url
 
 urlpatterns = [
-    url(r'^jobs_majors/$', 'contents.views.jobs_majors', name='jobs_majors'),
+    url(r'^jobs&majors/$', 'contents.views.jobs_majors', name='jobs_majors'),
+    url(r'^mentors/$', 'contents.views.mentors', name='mentors'),
+    url(r'^team/$', 'contents.views.team', name='team'),
+    url(r'^contact/$', 'contents.views.contact', name='contact'),
     url(r'^$', 'contents.views.home', name='home'),
 ]
--- contents/views.py
+++ contents/views.py
@@ -6,3 +6,12 @@ def home(request):
 
 def jobs_majors(request):
     return render(request, 'jobs_majors.html')
+
+def mentors(request):
+    return render(request, 'mentors.html')
+
+def team(request):
+    return render(request, 'team.html')
+
+def contact(request):
+    return render(request, 'contact.html')
