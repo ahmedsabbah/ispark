e3dca7ffa7d51cc56dcb874cf126a3f9345df8fb
Ahmed Sabbah <ahmedsabba7@gmail.com>
2016-08-18 05:59:15 +0200

    deployment setups

--- ispark/settings.py
+++ ispark/settings.py
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'ispark.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ispark_db',
+        'USER': 'enlighthost',
+        'PASSWORD': 'Pass123',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
@@ -139,7 +147,8 @@ MEDIA_URL = '/media/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = '/home/enlighthost/webapps/static/'
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
--- ispark/wsgi.py
+++ ispark/wsgi.py
@@ -8,8 +8,21 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 
-import os
+import os, sys, site
+
+site.addsitedir('/home/enlighthost/webapps/ispark/lib/python2.7/site-packages')
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ispark.settings")
+
+activate_this = os.path.expanduser("~/home/enlighthost/webapps/ispark/bin/activate_this.py")
+execfile(activate_this, dict(__file__=activate_this))
+
+# Calculate the path based on the location of the WSGI script
+project = '/home/enlighthost/webapps/ispark/'
+workspace = os.path.dirname(project)
+sys.path.append(workspace)
+
+sys.path = ['/home/enlighthost/webapps/ispark/ispark', '/home/enlighthost/webapps/ispark'] + sys.path
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
