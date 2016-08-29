264615e90b4f4d6ff2e2cdef937c88b7d1572a6f
Ahmed Sabbah <ahmedsabba7@gmail.com>
2016-08-29 18:08:20 +0200

    email server fix

--- ispark/settings.py
+++ ispark/settings.py
@@ -123,13 +123,21 @@ USE_TZ = True
 # EMAIL_PORT = 587
 # EMAIL_USE_TLS = True
 
-EMAIL_HOST = 'mail.isparkegypt.com'
-EMAIL_HOST_USER = 'ahmed.saeed@isparkegypt.com'
-EMAIL_HOST_PASSWORD = 'ispark2015'
-DEFAULT_FROM_EMAIL = 'ahmed.saeed@isparkegypt.com'
-SERVER_EMAIL = 'ahmed.saeed@isparkegypt.com'
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
+# EMAIL_HOST = 'mail.isparkegypt.com'
+# EMAIL_HOST_USER = 'ahmed.saeed@isparkegypt.com'
+# EMAIL_HOST_PASSWORD = 'ispark2015'
+# DEFAULT_FROM_EMAIL = 'ahmed.saeed@isparkegypt.com'
+# SERVER_EMAIL = 'ahmed.saeed@isparkegypt.com'
+# EMAIL_PORT = 25
+# EMAIL_USE_TLS = False
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.privateemail.com'
+EMAIL_PORT=465
+EMAIL_HOST_USER='info@isparkegypt.com'
+EMAIL_HOST_PASSWORD='ispark2015'
+EMAIL_USE_SSL=True
+EMAIL_USE_TLS=True
 
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
