437649ff07070fc547e32c53db00b21978bc0de6
Ahmed Sabbah <ahmedsabba7@gmail.com>
2016-10-12 17:32:28 +0200

    slider join now fix and debug to false

--- ispark/settings.py
+++ ispark/settings.py
@@ -24,7 +24,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "2x=^s1jc8hi4i$&m_ea_g!ic1pg99*-cl+3$v=jbe0tubkfqo2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
