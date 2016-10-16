3892f11cc4b6d25cc0a2b730ebb32bd22eab5ce6
Ahmed Sabbah <ahmedsabba7@gmail.com>
2016-10-16 16:43:12 +0200

    forms url fixed

--- services/urls.py
+++ services/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     url(r'^profile/$', 'services.views.profile', name='profile'),
     url(r'^internships/$', 'services.views.internships', name='internships'),
     url(r'^internship/(?P<pk>[0-9]+)/$', 'services.views.internship', name='internship'),
-    url(r'^tour/(?P<pk>[0-9]+)/apply$', 'services.views.tour_apply', name='tour_apply'),
-    url(r'^conference/(?P<pk>[0-9]+)/apply$', 'services.views.conference_apply', name='conference_apply'),
-    url(r'^internship/(?P<pk>[0-9]+)/apply$', 'services.views.internship_apply', name='internship_apply'),
+    url(r'^tour/(?P<pk>[0-9]+)/apply/$', 'services.views.tour_apply', name='tour_apply'),
+    url(r'^conference/(?P<pk>[0-9]+)/apply/$', 'services.views.conference_apply', name='conference_apply'),
+    url(r'^internship/(?P<pk>[0-9]+)/apply/$', 'services.views.internship_apply', name='internship_apply'),
     url(r'^get_majors', 'services.views.get_majors', name='get_majors')
 ]
